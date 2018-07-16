10ef9d23ebb31e449e03d2c5c82cc094ac4b213a
lujx_1024 <lujx_1024@163.com>
2018-07-16 14:31:41 +0800

    新增了Connectivity类的selectLampBySn()方法,该方法是接收前段传递的机器人sn号，查询数据表中的对应记录并返回

--- src/org/sgpro/signalmaster/Connectivity.java
+++ src/org/sgpro/signalmaster/Connectivity.java
@@ -7,11 +7,10 @@ package org.sgpro.signalmaster;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -29,12 +29,8 @@ import javax.ws.rs.core.MediaType;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.hibernate.Transaction;
-import org.jboss.logging.Param;
 import org.sgpro.db.HibSf;
-import org.sgpro.db.ViewRemoteRobotList;
-import org.sgpro.db.ViewRemoteSelectUserinfoList;
 import org.sgpro.util.DateUtil;
-import org.sgpro.util.Digest;
 
 @Path("/connect")
 public class Connectivity extends HttpServlet {
@@ -66,7 +62,7 @@ public class Connectivity extends HttpServlet {
 	}
 
 	/**
-	 * 查询机器人所管控的左右灯具
+	 * 根据机器人名称查询机器人所管控的所有灯具
 	 * 
 	 * @param robotName
 	 *            机器人名称
@@ -76,17 +72,13 @@ public class Connectivity extends HttpServlet {
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	public String selectLamp(@FormParam("robotName") String robotName) {
-
 		// 返回结果
 		Result r = Result.success();
 
-		// Transaction trans = null;
 		Connection connection = null;
 
 		CallableStatement callable = null;
 		ResultSet rs = null;
-
-		// trans = HibSf.getSession().beginTransaction();
 
 		// 结果集容器
 		List<Map<String, Object>> controlInfo = new ArrayList<>();
@@ -104,15 +96,83 @@ public class Connectivity extends HttpServlet {
 		if (!robotNameIsNull) {
 			sql += " AND r.NAME LIKE ? ";
 		}
+		connection = HibSf.getConnection();
 		try {
-			connection = HibSf.getConnection();
 			callable = connection.prepareCall(sql);
 
 			// 若机器人名称不为空，则拼接模糊查询
 			if (!robotNameIsNull) {
 				callable.setString(1, "%" + robotName + "%");
 			}
+			// 获取查询结果
+			rs = callable.executeQuery();
+			// 迭代取结果
+			while (rs.next()) {
+				Map<String, Object> map = new HashMap<>();
+				map.put("robotSn", rs.getString("robotSn"));
+				map.put("robotName", rs.getString("robotName"));
+				map.put("lambName", rs.getString("lambName"));
+				map.put("lambIndex", rs.getInt("lambIndex"));
+				map.put("lambStatus", rs.getInt("lambStatus"));
+				map.put("id", rs.getInt("id"));
+				map.put("deviceId", rs.getString("deviceId"));
+				controlInfo.add(map);
+			}
+			System.err.println(controlInfo);
+			r.setData(controlInfo);
 
+		} catch (SQLException e) {
+			r = Result.unknowException(e);
+			e.printStackTrace();
+		} finally {
+			// 关闭所有连接
+			closeConnectionObject(rs, callable, connection);
+		}
+		return r.toString();
+	}
+	/**
+	 * 根据机器人Sn号查询机器人所管控的左右灯具
+	 * 
+	 * @param robotSn
+	 *            机器人名称
+	 * @return 机器人的灯具控制信息
+	 */
+	@Path("selectlamp/{robotSn}")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public String selectLampBySn(@PathParam("robotSn") String robotSn) {
+		// 返回结果
+		Result r = Result.success();
+
+		Connection connection = null;
+
+		CallableStatement callable = null;
+		ResultSet rs = null;
+
+		// 结果集容器
+		List<Map<String, Object>> controlInfo = new ArrayList<>();
+
+		boolean robotNameIsNull = true;
+
+		if (!StringUtils.isEmpty(robotSn)) {
+			robotNameIsNull = false;
+		}
+
+		// 拼接字符串
+		String sql = "SELECT c.ID id ,c.DEVICE_ID deviceId, c.ROBOT_SN robotSn,r.NAME robotName,c.LAMP_NAME lambName,c.LAMP_INDEX lambIndex,c.STATUS lambStatus FROM [dbo].[ENT_ROBOT] r,dbo.ENT_CONNECTIVITY_LAMP c where c.ROBOT_SN=r.IMEI";
+
+		// 若机器人名称不为空，则拼接模糊查询
+		if (!robotNameIsNull) {
+			sql += " AND c.ROBOT_SN LIKE ? ";
+		}
+		try {
+			connection = HibSf.getConnection();
+			callable = connection.prepareCall(sql);
+
+			// 若机器人名称不为空，则拼接模糊查询
+			if (!robotNameIsNull) {
+				callable.setString(1, "%" + robotSn + "%");
+			}
 			// 获取查询结果
 			rs = callable.executeQuery();
 
@@ -135,51 +195,14 @@ public class Connectivity extends HttpServlet {
 		} catch (SQLException e) {
 			r = Result.unknowException(e);
 			e.printStackTrace();
-
-			// try {
-			// if (connection != null) {
-			// connection.rollback();
-			// }
-			//// if (trans != null) {
-			//// trans.rollback();
-			//// }
-			// } catch (Throwable t) {
-			// r = Result.unknowException(t);
-			// t.printStackTrace();
-			// }
 		} finally {
 			// 关闭所有连接
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			if (callable != null) {
-				try {
-					callable.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-
-			if (connection != null) {
-				try {
-					connection.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+			closeConnectionObject(rs, callable, connection);
 
 		}
 		return r.toString();
 
 	}
-
 	/**
 	 * 增加灯路信号信息
 	 */
@@ -291,6 +314,39 @@ public class Connectivity extends HttpServlet {
 		}
 		return r.toString();
 
+	}
+	
+	/**
+	 * 关闭数据连接参数
+	 * @param rs 结果集
+	 * @param ps  preparedStatement
+	 * @param conn 数据库连接
+ 	 */
+	public void closeConnectionObject(ResultSet rs,PreparedStatement ps,Connection conn){
+		if(rs!=null){
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(ps!=null){
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(conn!=null){
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
